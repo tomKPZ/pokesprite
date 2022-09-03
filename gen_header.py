b0b491d67622239e71b25506e523cc95c2b87b4b
tomKPZ <tomKPZ@gmail.com>
2022-09-02 22:16:14 -0700

    allow combining sprites from multiple games

--- gen_header.py
+++ gen_header.py
@@ -2,24 +2,28 @@
 
 import collections
 import os
-import re
 import sys
 
 import PIL.Image
 
 URL = "https://pokemondb.net/sprites"
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-SPRITES_DIR = os.path.join(
+VERSIONS_DIR = os.path.join(
     SCRIPT_DIR,
     "sprites",
     "sprites",
     "pokemon",
     "versions",
-    "generation-iii",
-    "emerald",
 )
-SHINY_DIR = os.path.join(SPRITES_DIR, "shiny")
-FNAME_PAT = re.compile(r"\d{1,3}.png")
+SPRITES = [
+    ("generation-iii", "emerald", 386),
+    # ("generation-iii", "firered-leafgreen", 151),
+    # ("generation-iii", "ruby-sapphire", 386),
+    # ("generation-iv", "diamond-pearl", 493),
+    # ("generation-iv", "heartgold-soulsilver", 493),
+    # ("generation-iv", "platinum", 493),
+    # ("generation-v", "black-white", 650),
+]
 
 
 def pixel(sprite, x, y):
@@ -40,31 +44,34 @@ def create_colormap(sprite, shiny):
     return colormap
 
 
-for p in os.listdir(SPRITES_DIR):
-    fname = os.path.join(SPRITES_DIR, p)
-    if not FNAME_PAT.match(p):
-        continue
-    sprite = PIL.Image.open(fname).convert("RGBA")
-    shiny = PIL.Image.open(os.path.join(SHINY_DIR, p)).convert("RGBA")
+for gen, game, max_id in SPRITES:
+    sprites_dir = os.path.join(VERSIONS_DIR, gen, game)
+    shiny_dir = os.path.join(sprites_dir, "shiny")
+    for id in range(max_id):
+        basename = "%d.png" % (id + 1)
+        path = os.path.join(sprites_dir, basename)
+        sprite = PIL.Image.open(path).convert("RGBA")
+        shiny_path = os.path.join(shiny_dir, basename)
+        shiny = PIL.Image.open(shiny_path).convert("RGBA")
 
-    colormap = create_colormap(sprite, shiny)
-    if len(colormap) > 16:
-        print("Excess colors in sprite", fname, file=sys.stderr)
-        continue
+        colormap = create_colormap(sprite, shiny)
+        if len(colormap) > 16:
+            print("Excess colors in sprite", path, file=sys.stderr)
+            continue
 
-    xl, yl, xh, yh = sprite.getbbox()
-    print("{%d,%d,(uint8_t[]){" % (xh - xl, yh - yl), end="")
-    startb = True
-    for y in range(yl, yh):
-        for x in range(xl, xh):
-            color = (pixel(sprite, x, y), pixel(shiny, x, y))
-            color = colormap[color]
-            print(("0x%X" if startb else "%X,") % color, end="")
-            startb = not startb
-    print("},{")
-    for (color, _) in colormap:
-        print("0x%04X," % color, end="")
-    print("},{")
-    for (_, color) in colormap:
-        print("0x%04X," % color, end="")
-    print("}},")
+        xl, yl, xh, yh = sprite.getbbox()
+        print("{%d,%d,(uint8_t[]){" % (xh - xl, yh - yl), end="")
+        startb = True
+        for y in range(yl, yh):
+            for x in range(xl, xh):
+                color = (pixel(sprite, x, y), pixel(shiny, x, y))
+                color = colormap[color]
+                print(("0x%X" if startb else "%X,") % color, end="")
+                startb = not startb
+        print("},{")
+        for (color, _) in colormap:
+            print("0x%04X," % color, end="")
+        print("},{")
+        for (_, color) in colormap:
+            print("0x%04X," % color, end="")
+        print("}},")
