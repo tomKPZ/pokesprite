219642c78b3ab7e2d58f60fcc1f80af652aa9384
tomKPZ <tomKPZ@gmail.com>
2022-09-03 19:05:47 -0700

    Compress sprites

--- gen_header.py
+++ gen_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import collections
+import heapq
+import itertools
 import os
 import sys
 
@@ -44,6 +46,86 @@ def create_colormap(sprite, shiny):
     return colormap
 
 
+def rle(data):
+    counts = []
+    runs = []
+    for x, g in itertools.groupby(data):
+        count = len(list(g))
+        while count > 16:
+            counts.append(15)
+            runs.append(x)
+            count -= 16
+        counts.append(count - 1)
+        runs.append(x)
+    return (counts, runs)
+
+
+def he(data):
+    counter = collections.Counter(data)
+    heap = [(counter[i], i, i) for i in range(16)]
+    heapq.heapify(heap)
+    while len(heap) > 1:
+        c1, _, v1 = heapq.heappop(heap)
+        c2, _, v2 = heapq.heappop(heap)
+        heapq.heappush(heap, (c1 + c2, -len(heap), (v1, v2)))
+    tree = heap[0][2]
+
+    data2bits = {}
+    acc = []
+    form = []
+    perm = []
+
+    def dfs(node):
+        if type(node) == int:
+            form.append(1)
+            data2bits[node] = acc[::]
+            perm.append(node)
+            return
+        form.append(0)
+        l, r = node
+        acc.append(0)
+        dfs(l)
+        acc.pop()
+        acc.append(1)
+        dfs(r)
+        acc.pop()
+
+    dfs(tree)
+    bits = [y for x in data for y in data2bits[x]]
+    return (bits, form, perm)
+
+
+def bit_encode(bits):
+    encoded = 0
+    for bit in bits:
+        encoded *= 2
+        encoded += bit
+    return encoded
+
+
+def byte_encode(bits):
+    while len(bits) % 8 != 0:
+        bits.append(0)
+    print("{")
+    for i in range(0, len(bits), 8):
+        print("0x%02X," % bit_encode(bits[i : i + 8]), end="")
+    print("},")
+
+
+def output_huffman(data):
+    bits, form, perm = he(data)
+    print("{")
+    byte_encode(list(form))
+    print("{")
+    startb = True
+    for x in perm:
+        print(("0x%X" if startb else "%X,") % x, end="")
+        startb = not startb
+    print("},(uint8_t[])")
+    byte_encode(bits)
+    print("},")
+
+
 for gen, game, max_id, has_shiny in SPRITES:
     sprites_dir = os.path.join(VERSIONS_DIR, gen, game)
     shiny_dir = os.path.join(sprites_dir, "shiny")
@@ -62,19 +144,21 @@ for gen, game, max_id, has_shiny in SPRITES:
             print("Excess colors in sprite", path, file=sys.stderr)
             continue
 
-        startb = True
+        image = []
         xl, yl, xh, yh = sprite.getbbox()
-        print("{%d,%d,(uint8_t[]){" % (xh - xl, yh - yl), end="")
         for y in range(yl, yh):
             for x in range(xl, xh):
                 color = (pixel(sprite, x, y), pixel(shiny, x, y))
-                color = colormap[color]
-                print(("0x%X" if startb else "%X,") % color, end="")
-                startb = not startb
-        print("},{")
+                image.append(colormap[color])
+
+        print("{%d,%d,{" % (xh - xl, yh - yl), end="")
         for (color, _) in colormap:
             print("0x%04X," % color, end="")
         print("},{")
         for (_, color) in colormap:
             print("0x%04X," % color, end="")
-        print("}},")
+        print("},")
+        counts, runs = rle(image)
+        output_huffman(counts)
+        output_huffman(runs)
+        print("},")
--- yapsit.c
+++ yapsit.c
@@ -10,25 +10,120 @@
 #define FG "\033[38;2;%d;%d;%dm"
 #define BG "\033[48;2;%d;%d;%dm"
 
-struct Sprite {
-  unsigned int w;
-  unsigned int h;
-  const uint8_t *image;
-  const uint16_t colormap[16];
-  const uint16_t shiny[16];
-} const sprites[] = {
+typedef struct {
+  uint8_t form[4];
+  uint8_t perm[8];
+  const uint8_t *data;
+} HuffmanHeader;
+
+typedef struct {
+  bool is_leaf;
+  union {
+    struct {
+      uint8_t l;
+      uint8_t r;
+    } node;
+    uint8_t val;
+  } data;
+} HuffmanNode;
+
+typedef struct {
+  const uint8_t *bits;
+  size_t offset;
+} BitstreamContext;
+
+typedef struct {
+  HuffmanNode nodes[31];
+  BitstreamContext bits;
+} HuffmanContext;
+
+typedef struct {
+  uint8_t count;
+  uint8_t value;
+  HuffmanContext counts;
+  HuffmanContext values;
+} RunlengthContext;
+
+typedef struct {
+  uint8_t w;
+  uint8_t h;
+  uint16_t colormap[16];
+  uint16_t shiny[16];
+  HuffmanHeader counts;
+  HuffmanHeader values;
+} Sprite;
+
+static bool read_bit(BitstreamContext *bitstream) {
+  uint8_t byte = bitstream->bits[bitstream->offset / 8];
+  bool bit = byte & (1 << (7 - bitstream->offset % 8));
+  bitstream->offset += 1;
+  return !!bit;
+}
+
+static uint8_t decode_node(BitstreamContext *bits, HuffmanNode *nodes,
+                           uint8_t i, uint8_t **perm) {
+  if (read_bit(bits)) {
+    nodes[i].is_leaf = true;
+    nodes[i].data.val = **perm;
+    (*perm)++;
+    return i;
+  }
+  uint8_t l = decode_node(bits, nodes, i, perm);
+  uint8_t r = decode_node(bits, nodes, l + 1, perm);
+  i = r + 1;
+  nodes[i].is_leaf = false;
+  nodes[i].data.node.l = l;
+  nodes[i].data.node.r = r;
+  return i;
+}
+
+static void huffman_init(HuffmanContext *context, const HuffmanHeader *header) {
+  uint8_t perms[16];
+  for (int i = 0; i < 8; i++) {
+    perms[2 * i] = header->perm[i] >> 4;
+    perms[2 * i + 1] = header->perm[i] & 0x0F;
+  }
+  BitstreamContext bits = {header->form, 0};
+  uint8_t *perm = perms;
+  decode_node(&bits, context->nodes, 0, &perm);
+  context->bits.bits = header->data;
+  context->bits.offset = 0;
+}
+
+static uint8_t huffman_decode(HuffmanContext *context) {
+  HuffmanNode *node = &context->nodes[30];
+  while (!node->is_leaf) {
+    if (read_bit(&context->bits))
+      node = &context->nodes[node->data.node.r];
+    else
+      node = &context->nodes[node->data.node.l];
+  }
+  return node->data.val;
+}
+
+static void runlength_init(RunlengthContext *context,
+                           const HuffmanHeader *counts,
+                           const HuffmanHeader *values) {
+  context->count = 0;
+  context->value = 0;
+  huffman_init(&context->counts, counts);
+  huffman_init(&context->values, values);
+}
+
+static uint8_t runlength_decode(RunlengthContext *context) {
+  if (!context->count) {
+    context->count = huffman_decode(&context->counts) + 1;
+    context->value = huffman_decode(&context->values);
+  }
+  context->count--;
+  return context->value;
+}
+
+static const Sprite sprites[] = {
 #include "pokemon.h"
 };
 
 const size_t n_sprites = sizeof(sprites) / sizeof(sprites[0]);
-
-static uint8_t pixel(const struct Sprite *sprite, size_t x, size_t y) {
-  size_t i = y * sprite->w + x;
-  if (i >= sprite->w * sprite->h)
-    return 0;
-  uint8_t val = sprite->image[i / 2];
-  return i % 2 == 0 ? val >> 4 : val & 0x0F;
-}
 
 static bool A(uint16_t c) { return c >> 15; }
 static uint8_t R(uint16_t c) { return ((c >> 10) & 0b11111) * 8 * 255 / 248; }
@@ -42,7 +137,7 @@ int main() {
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
   size_t n = 0;
-  const struct Sprite *sprite = NULL;
+  const Sprite *sprite = NULL;
   for (size_t i = 0; i < n_sprites; i++) {
     if (sprites[i].w > w.ws_col || (sprites[i].h + 1) / 2 + 2 > w.ws_row)
       continue;
@@ -55,10 +150,17 @@ int main() {
   const uint16_t *colormap =
       rand() % 16 == 0 ? sprite->shiny : sprite->colormap;
 
+  RunlengthContext t;
+  runlength_init(&t, &sprite->counts, &sprite->values);
   for (size_t y = 0; y < sprite->h; y += 2) {
+    RunlengthContext b = t;
+    for (size_t x = 0; x < sprite->w; x++)
+      runlength_decode(&b);
     for (size_t x = 0; x < sprite->w; x++) {
-      uint16_t h = colormap[pixel(sprite, x, y)];
-      uint16_t l = colormap[pixel(sprite, x, y + 1)];
+      uint16_t h = colormap[runlength_decode(&t)];
+      uint16_t l = 0;
+      if (y + 1 < sprite->h)
+        l = colormap[runlength_decode(&b)];
       if (A(h) && A(l))
         printf(BG FG "▄", R(h), G(h), B(h), R(l), G(l), B(l));
       else if (A(h))
@@ -69,6 +171,7 @@ int main() {
         printf(" ");
       printf("\033[m");
     }
+    t = b;
     printf("\n");
   }
   return 0;
