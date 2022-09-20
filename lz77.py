acaf44de420c96af6ae315ff17edf81600076b41
tomKPZ <tomKPZ@gmail.com>
2022-09-20 10:18:10 -0700

    simplify lz77

--- lz77.py
+++ lz77.py
@@ -2,7 +2,6 @@
 
 import cProfile
 import random
-import sys
 
 
 def lz77(data):
@@ -11,22 +10,21 @@ def lz77(data):
 
     dp = [0] * n
     for i in reversed(range(n)):
-        runs = {}
+        ans = (inf, None)
         for j in range(i):
             for k in range(j, min(i, n - i + j)):
                 if data[k] != data[i + k - j]:
                     break
-                runs[k - j + 1] = i - j
-        if not runs:
+                runlen = k - j + 1
+                delta = i - j
+                index = i + runlen + 1
+                lstlen, lst = dp[index] if index < n else (0, None)
+                nxt = data[i + runlen] if i + runlen < n else None
+                ans = min(ans, (1 + lstlen, ((delta, runlen, nxt), lst)))
+        if not ans[1]:
             size, lst = dp[i + 1] if i + 1 < n else (0, None)
             dp[i] = (size + 1, ((0, 0, data[i]), lst))
             continue
-        ans = (inf,)
-        for runlen, delta in runs.items():
-            j = i + runlen + 1
-            lstlen, lst = dp[j] if j < n else (0, None)
-            nxt = data[i + runlen] if i + runlen < n else None
-            ans = min(ans, (1 + lstlen, ((delta, runlen, nxt), lst)))
         dp[i] = ans
 
     node = dp[0][1]
@@ -59,5 +57,4 @@ for _ in range(10):
     string = "".join(random.choice("ab") for _ in range(500))
     test(string)
 str = "".join(random.choice("ab") for _ in range(500))
-sys.setrecursionlimit(10000)
 cProfile.run("lz77(str)")
